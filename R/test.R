7dfad234e64fd66c7e867c52665ef9bd3f163269
labuser <labuser@biolab1013.local>
2017-01-31 11:40:23 -0800

    changed the test

--- R/test.R
+++
@@ -1,4 +0,0 @@
-test_that("hi() only accepts appropriate 'who'",{
-  expect_equal(hi("Martin"),"hi MARTIN how are you?")
-  expect_equal(hi(123),Error)
-})
--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,4 +1,3 @@
 test_that("hi() only accepts appropriate 'who'",{
   expect_equal(hi("Martin"),"hi MARTIN how are you?")
-  expect_equal(hi(123),Error)
 })
