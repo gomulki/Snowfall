e5de0519cb23cd34f1e06d5f574e73ac81422448
labuser <labuser@biolab1013.local>
2017-01-31 11:47:54 -0800

    test `how` argument

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -1,3 +1,9 @@
 test_that("hi() only accepts appropriate 'who'",{
   expect_equal(hi("Martin"),"hi MARTIN how are you?")
 })
+
+test_that("hi() only accepts appropriate 'how'",{
+  expect_equal(hi("Dick",how="whisper"),"hi dick how are you?")
+  expect_equal(hi("DiCk",how="asis"),"hi DiCk how are you?")
+  expect_error(hi("DiCk",how="silent"))
+})
